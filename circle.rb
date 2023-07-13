da0d9784555938fb04898de97a42636a00ecbf10
aycelacap <[raycel5lacap@gmail.com]>
2023-07-13 11:11:00 -0400

    Add #hello to Circle class

--- circle.rb
+++ circle.rb
@@ -1,7 +1,14 @@
 class Circle 
+  attr_reader :radius
 
-  def initalize(radius)
+  def initalize(radius, x, y)
     @radius = radius
+    @x = x 
+    @y = y
+  end
+
+  def hello 
+    p "hello"
   end
 
 end
